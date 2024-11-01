b31225e40c0ca1902d058264183dd168f8f58b7d
ThienHuynh0210 <phuocthien021003@gmail.com>
2024-11-01 11:28:56 +0700

    authen

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const { auth } = require("../middleware/authMiddleware");
+const { getUsers, createUser, loginUser, logoutUser } = require("../controllers/userController");
+
+const router = Router();
+
+router.get("/", auth, getUsers);
+
+router.post("/", createUser);
+router.post("/login", loginUser);
+router.get("/logout", logoutUser);
+
+module.exports = router;
